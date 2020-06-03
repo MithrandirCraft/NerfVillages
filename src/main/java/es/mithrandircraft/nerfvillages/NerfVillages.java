89cfdbe7ef7c0b48c3fa39438c8963e112f4fee7
Mangonels <dylancalaflatham@gmail.com>
2020-06-03 19:30:34 +0200

    FIRST!

---
+++ src/main/java/es/mithrandircraft/nerfvillages/NerfVillages.java
@@ -0,0 +1,17 @@
+package es.mithrandircraft.nerfvillages;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NerfVillages extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    } //quack
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
