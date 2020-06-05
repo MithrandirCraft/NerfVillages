91f080ef4afb1311c035727668fdf5672113bb30
Mangonels <dylancalaflatham@gmail.com>
2020-06-05 17:33:04 +0200

    Trying to stop replenish

--- src/main/java/es/mithrandircraft/nerfvillages/NerfVillages.java
+++ src/main/java/es/mithrandircraft/nerfvillages/NerfVillages.java
@@ -1,17 +1,22 @@
 package es.mithrandircraft.nerfvillages;
 
+import es.mithrandircraft.nerfvillages.events.VillagerReplenishTradeEv;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class NerfVillages extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        //Config load:
+        getConfig().options().copyDefaults();
+        saveDefaultConfig();
 
-    } //quack
+        //Event registring
+        getServer().getPluginManager().registerEvents(new VillagerReplenishTradeEv(this), this);
+    }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+
     }
 }
---
+++ src/main/java/es/mithrandircraft/nerfvillages/events/VillagerReplenishTradeEv.java
@@ -0,0 +1,26 @@
+package es.mithrandircraft.nerfvillages.events;
+
+import es.mithrandircraft.nerfvillages.NerfVillages;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.VillagerReplenishTradeEvent;
+
+public class VillagerReplenishTradeEv implements Listener {
+
+    private NerfVillages mainClassAccess;
+
+    public VillagerReplenishTradeEv(NerfVillages mca)
+    {
+        mainClassAccess = mca;
+    }
+
+    @EventHandler
+    public void replenishEv(VillagerReplenishTradeEvent e)
+    {
+        System.out.println("replenish event triggered.");
+        if(mainClassAccess.getConfig().getBoolean("DisableVillagerRestock")){
+            System.out.println("cancelling event.");
+            e.setCancelled(true);
+        }
+    }
+}
