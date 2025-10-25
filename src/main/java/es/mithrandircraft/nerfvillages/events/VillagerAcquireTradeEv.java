ce0be9fc3528cef2e9efaac534df04dedcf853a9
Mangonels <dylancalaflatham@gmail.com>
2025-10-26 01:34:40 +0200

    Only mending and thorns books get nerfed

--- src/main/java/es/mithrandircraft/nerfvillages/events/VillagerAcquireTradeEv.java
+++ src/main/java/es/mithrandircraft/nerfvillages/events/VillagerAcquireTradeEv.java
@@ -3,10 +3,12 @@ package es.mithrandircraft.nerfvillages.events;
 import es.mithrandircraft.nerfvillages.NerfVillages;
 import java.util.concurrent.ThreadLocalRandom;
 import org.bukkit.Material;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.VillagerAcquireTradeEvent;
+import org.bukkit.inventory.meta.EnchantmentStorageMeta;
 
 public class VillagerAcquireTradeEv implements Listener {
     private final NerfVillages mainClassAccess;
@@ -17,13 +19,20 @@ public class VillagerAcquireTradeEv implements Listener {
 
     @EventHandler(priority = EventPriority.LOW)
     public void acquireEv(VillagerAcquireTradeEvent e) {
-        if (e.getRecipe().getResult().getType() == Material.ENCHANTED_BOOK && this.mainClassAccess.getConfig().getBoolean("ActivateEnchantedBookChance")) {
-            int randomInRange = ThreadLocalRandom.current().nextInt(1, this.mainClassAccess.getConfig().getInt("EnchantedBookChance"));
-            if (randomInRange == 1) {
-                e.getRecipe().setMaxUses(1);
-            } else {
-                e.getRecipe().setMaxUses(0);
+        if (e.getRecipe().getResult().getType() == Material.ENCHANTED_BOOK) {
+            EnchantmentStorageMeta bookMeta = (EnchantmentStorageMeta) e.getRecipe().getResult().getItemMeta();
+            boolean hasMendingOrThorns = bookMeta.hasStoredEnchant(Enchantment.MENDING) ||
+                                         bookMeta.hasStoredEnchant(Enchantment.THORNS);
+
+            if (hasMendingOrThorns && this.mainClassAccess.getConfig().getBoolean("ActivateThornsAndMendingBookChance")) {
+                int randomInRange = ThreadLocalRandom.current().nextInt(1, this.mainClassAccess.getConfig().getInt("ThornsAndMendingBookChance"));
+                if (randomInRange == 1) {
+                    e.getRecipe().setMaxUses(1);
+                } else {
+                    e.getRecipe().setMaxUses(0);
+                }
             }
+            // Other enchanted books remain unnerfed - do nothing
         } else {
             e.getRecipe().setMaxUses((int)Math.ceil((e.getRecipe().getMaxUses() / this.mainClassAccess.getConfig().getInt("MaxTradesNerfDivider"))));
         }
